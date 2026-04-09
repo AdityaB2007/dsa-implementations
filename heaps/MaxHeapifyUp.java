b36afb5c05d29c6b9f6d3cdf519020e873c5b71d
Aditya Baisakh <adityabaisakh123@gmail.com>
2026-04-08 21:40:01 -0500

    feat: implement MaxHeapifyUp.java

---
+++ heaps/MaxHeapifyUp.java
@@ -0,0 +1,18 @@
+public class MaxHeapifyUp {
+    public static void heapifyUp(int[] heap, int index)
+    {
+        while (index > 0)
+        {
+            int parent = HeapUtil.parent(index);
+            if (heap[index] > heap[parent])
+            {
+                HeapUtil.swap(heap, index, parent);
+                index = parent;
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
+}
