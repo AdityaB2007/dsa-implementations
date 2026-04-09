58a2d1d572ec440642d1f75423420b56bc2bdd65
Aditya Baisakh <adityabaisakh123@gmail.com>
2026-04-08 21:52:13 -0500

    feat: implement MinHeapifyUp.java

---
+++ heaps/MinHeapifyUp.java
@@ -0,0 +1,19 @@
+public class MinHeapifyUp
+{
+    public static void heapifyUp(int[] heap, int index)
+    {
+        while (index > 0)
+        {
+            int parent = HeapUtil.parent(index);
+            if (heap[index] < heap[parent])
+            {
+                HeapUtil.swap(heap, index, parent);
+                index = parent;
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
+}
